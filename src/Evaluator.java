f00297ef3ce39a8996117231bd4e4643f58b13a3
Stick-2357 <emonnig10@gmail.com>
2021-04-07 20:06:24 -0500

    temp fix Evaluator and Interpreter errors

--- src/Evaluator.java
+++ src/Evaluator.java
@@ -1,40 +1,51 @@
 package src;
+
 import java.util.*;
 
-/* 
+/*
  * Only +, * operators with integers
- * So all values are still integers 
+ * So all values are still integers
  */
- public class Evaluator {
+public class Evaluator {
 
-//	static final boolean debug = true;
-	static final boolean debug = false;
-	static Integer evalS(String expression) {
-		// return true if s is a valid expression. expression could be nested
-		ArrayList<String> tokens = Tokenizer.tokenize(expression);
-		if(debug) tokens.forEach(System.out::println);
-		return evalExpr(tokens, 0, tokens.size()-1);	
-	}
-	static Integer evalExpr(ArrayList<String> tokens, int start, int end) {
-		// add your code
-	}
-	static ArrayList<Integer> getOperands(ArrayList<String> tokens, int start, int end) {
-		// add your code
-	}
-	static Integer evalId(ArrayList<String> tokens, int start, int end) {
-		// add your code
-	}
-	static Integer evalInteger(ArrayList<String> tokens, int start, int end) {
-		// add your code
-	}
-	public static void main(String[] args) {
-		System.out.println(evalS("234")); // 234
-		System.out.println(evalS("(+ 20)")); // 20
-		System.out.println(evalS("(+ 1 234)")); // 235
-		System.out.println(evalS("(+ 2 10 200)")); // 212
-		System.out.println(evalS("(* (+ 1 2) (+ 1 3))")); // 12 
-		System.out.println(evalS("(* (+ 1 2) (+ 1 3) (* 2 3))")); // 72 
+    //	static final boolean debug = true;
+    static final boolean debug = false;
 
-		System.out.println(evalS("(+ 20")); // null
-	}
+    static Integer evalS(String expression) {
+        // return true if s is a valid expression. expression could be nested
+        ArrayList<String> tokens = Tokenizer.tokenize(expression);
+        if (debug) tokens.forEach(System.out::println);
+        return evalExpr(tokens, 0, tokens.size() - 1);
+    }
+
+    static Integer evalExpr(ArrayList<String> tokens, int start, int end) {
+        // add your code
+        return 0;
+    }
+
+    static ArrayList<Integer> getOperands(ArrayList<String> tokens, int start, int end) {
+        // add your code
+        return new ArrayList<>(List.of(0));
+    }
+
+    static Integer evalId(ArrayList<String> tokens, int start, int end) {
+        // add your code
+        return 0;
+    }
+
+    static Integer evalInteger(ArrayList<String> tokens, int start, int end) {
+        // add your code
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(evalS("234")); // 234
+        System.out.println(evalS("(+ 20)")); // 20
+        System.out.println(evalS("(+ 1 234)")); // 235
+        System.out.println(evalS("(+ 2 10 200)")); // 212
+        System.out.println(evalS("(* (+ 1 2) (+ 1 3))")); // 12
+        System.out.println(evalS("(* (+ 1 2) (+ 1 3) (* 2 3))")); // 72
+
+        System.out.println(evalS("(+ 20")); // null
+    }
 }
--- src/Interpreter.java
+++ src/Interpreter.java
@@ -1,4 +1,5 @@
 package src;
+
 import java.util.*;
 
 /*
@@ -10,70 +11,86 @@ A Grammar for scheme application with integers and arithmetic operators +, *
 <id> -> <integer>  | <var>
  */
 
- public class Interpreter {
-	static boolean debug = false;
-	static Scanner scan;
-	static HashMap<String,Integer> variables = new HashMap<String, Integer>();
-	static boolean interpret(String s) {
-		// <S> -> <expr> | <define>
-		ArrayList<String> tokens = Tokenizer.tokenize(s);	
-		return (
-				define(tokens, 0, tokens.size()-1) ||
-				evaluate(tokens, 0, tokens.size()-1) 
-		);
-	}
-	static boolean define(ArrayList<String> tokens, int start, int end) {
-		// add your code
-	}
-	static boolean evaluate(ArrayList<String> tokens, int start, int end) {
-		Integer r = evalExpr(tokens, 0, tokens.size()-1);	
-		if (r == null) return false;
-		System.out.println(r);
-		return true;
-	}
-	static Integer evalExpr(ArrayList<String> tokens, int start, int end) {
-		// add your code
-	}
-	static ArrayList<Integer> getOperands(ArrayList<String> tokens, int start, int end) {
-		// add your code
-	}
-	static Integer evalId(ArrayList<String> tokens, int start, int end) {
-		// add your code
-	}
-	static Integer evalInteger(ArrayList<String> tokens, int start, int end) {
-		// add your code
-	}
-	static Integer evalVar(ArrayList<String> tokens, int start, int end) {
-		if(debug) System.out.println("evalVar("+start+","+end+")");
-		if(start!=end) return null;
-		String s = tokens.get(start);
-		if (isIdentifier(s)) {
-			Integer r = variables.get(s);
-			if(r == null)
-				System.out.println("Unknown symbol: "+s);
-			else return r;
-		}
-		return null;
-	}
-	static boolean isIdentifier(String s) {
-		// add your code
-	}
-	public static void main(String[] args) {
-		System.out.println("Andy Petite Chez Scheme 0.9. Nothing much supported.");
-		scan = new Scanner(System.in);
-		String userInput;
-		while(true) {
-			System.out.print("> ");
-			userInput = scan.nextLine().trim();
-			if(userInput.equals("")) 
-				continue;
-			else if (userInput.equals("(quit)"))
-				break;
-			else if (userInput.equals("(local)"))
-				System.out.println(variables);
-			else if (interpret(userInput) == false)
-				System.out.println("Syntax Error !!");
-		}
-		scan.close();
-	}
+public class Interpreter {
+    static boolean debug = false;
+    static Scanner scan;
+    static HashMap<String, Integer> variables = new HashMap<String, Integer>();
+
+    static boolean interpret(String s) {
+        // <S> -> <expr> | <define>
+        ArrayList<String> tokens = Tokenizer.tokenize(s);
+        return (
+                define(tokens, 0, tokens.size() - 1) ||
+                        evaluate(tokens, 0, tokens.size() - 1)
+        );
+    }
+
+    static boolean define(ArrayList<String> tokens, int start, int end) {
+        // add your code
+        return false;
+    }
+
+    static boolean evaluate(ArrayList<String> tokens, int start, int end) {
+        Integer r = evalExpr(tokens, 0, tokens.size() - 1);
+        if (r == null) return false;
+        System.out.println(r);
+        return true;
+    }
+
+    static Integer evalExpr(ArrayList<String> tokens, int start, int end) {
+        // add your code
+        return 0;
+    }
+
+    static ArrayList<Integer> getOperands(ArrayList<String> tokens, int start, int end) {
+        // add your code
+        return new ArrayList<>(List.of(0));
+    }
+
+    static Integer evalId(ArrayList<String> tokens, int start, int end) {
+        // add your code
+        return 0;
+    }
+
+    static Integer evalInteger(ArrayList<String> tokens, int start, int end) {
+        // add your code
+        return 0;
+    }
+
+    static Integer evalVar(ArrayList<String> tokens, int start, int end) {
+        if (debug) System.out.println("evalVar(" + start + "," + end + ")");
+        if (start != end) return null;
+        String s = tokens.get(start);
+        if (isIdentifier(s)) {
+            Integer r = variables.get(s);
+            if (r == null)
+                System.out.println("Unknown symbol: " + s);
+            else return r;
+        }
+        return null;
+    }
+
+    static boolean isIdentifier(String s) {
+        // add your code
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Andy Petite Chez Scheme 0.9. Nothing much supported.");
+        scan = new Scanner(System.in);
+        String userInput;
+        while (true) {
+            System.out.print("> ");
+            userInput = scan.nextLine().trim();
+            if (userInput.equals(""))
+                continue;
+            else if (userInput.equals("(quit)"))
+                break;
+            else if (userInput.equals("(local)"))
+                System.out.println(variables);
+            else if (interpret(userInput) == false)
+                System.out.println("Syntax Error !!");
+        }
+        scan.close();
+    }
 }
