35cf4784a08ef9897639cd2ca2d90c4d22df287e
Stick-2357 <emonnig10@gmail.com>
2021-04-07 19:28:56 -0500

    Remove space from tokens

--- src/Tokenizer.java
+++ src/Tokenizer.java
@@ -1,3 +1,5 @@
+package src;
+
 import java.util.*;
 
 public class Tokenizer {
@@ -9,11 +11,13 @@ public class Tokenizer {
         ArrayList<String> tokens = new ArrayList<>();
         StringBuilder token = new StringBuilder();
         TokenStatus status = TokenStatus.START;
-        String explicitTokens = "([)]' ";
+        String explicitTokens = "([)] '";
         for (char ch : s.toCharArray()) {
             if (status == TokenStatus.START) {
                 if (explicitTokens.contains(ch + "")) {
-                    tokens.add(ch + "");
+                    if (ch != ' ') {
+                        tokens.add(ch + "");
+                    }
                 } else {
                     token.append(ch);
                     status = TokenStatus.WORD;
@@ -21,9 +25,13 @@ public class Tokenizer {
             } else { // TokenStatus.WORD
                 if (explicitTokens.contains(ch + "")) {
                     tokens.add(token.toString());
-                    tokens.add(ch + "");
+                    if (ch != ' ') {
+                        tokens.add(ch + "");
+                    }
                     token = new StringBuilder();
                     status = TokenStatus.START;
+                } else if (ch == ' ') {
+                    // do nothing
                 } else {
                     token.append(ch);
                 }
@@ -35,18 +43,18 @@ public class Tokenizer {
 
     public static void main(String[] args) {
         tokenize("(+ 1 2)").forEach(System.out::println);
-        // tokenize("[=(+ 1 2) 3]").forEach(System.out::println);
-        // tokenize("(+ 2 3 1.5)").forEach(System.out::println);
-        // tokenize("(<= 1 2)").forEach(System.out::println);
-        // tokenize("(*(- 2 1)(+ 3 1.5))").forEach(System.out::println);
-        // tokenize("(* (- x 1) (- y 2))").forEach(System.out::println);
-        // tokenize("(/ 1.2e3 10)").forEach(System.out::println);
-        // tokenize("(*(* 1.2e-2 10) 10)").forEach(System.out::println);
-        // tokenize("(cons 12'(2 3))").forEach(System.out::println);
-        // tokenize("(equal? 1'abc)").forEach(System.out::println);
-        // tokenize("(define d^2 (+ (square (- x2 x1)) (square (- y2 y1)) ))").forEach(System.out::println);
-        // tokenize("(define 12a (+ 12 a_b)").forEach(System.out::println);
-        // tokenize("(define larger? (lambda (a b) (if (> a b) #t #f)))").forEach(System.out::println);
-        // tokenize("(cond [(< _t 12) 'morning] [(< _t 18) 'afternoon] [else 'evening])").forEach(System.out::println);
+        tokenize("[=(+ 1 2) 3]").forEach(System.out::println);
+        tokenize("(+ 2 3 1.5)").forEach(System.out::println);
+        tokenize("(<= 1 2)").forEach(System.out::println);
+        tokenize("(*(- 2 1)(+ 3 1.5))").forEach(System.out::println);
+        tokenize("(* (- x 1) (- y 2))").forEach(System.out::println);
+        tokenize("(/ 1.2e3 10)").forEach(System.out::println);
+        tokenize("(*(* 1.2e-2 10) 10)").forEach(System.out::println);
+        tokenize("(cons 12'(2 3))").forEach(System.out::println);
+        tokenize("(equal? 1'abc)").forEach(System.out::println);
+        tokenize("(define d^2 (+ (square (- x2 x1)) (square (- y2 y1)) ))").forEach(System.out::println);
+        tokenize("(define 12a (+ 12 a_b)").forEach(System.out::println);
+        tokenize("(define larger? (lambda (a b) (if (> a b) #t #f)))").forEach(System.out::println);
+        tokenize("(cond [(< _t 12) 'morning] [(< _t 18) 'afternoon] [else 'evening])").forEach(System.out::println);
     }
 }
