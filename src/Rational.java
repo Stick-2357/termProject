dce89733256e8d558cc9176aa10b65021b7ebf3c
Eli Monnig <35583627+Stick-2357@users.noreply.github.com>
2021-04-21 19:26:37 -0500

    remove dead constructor

--- src/Rational.java
+++ src/Rational.java
@@ -24,34 +24,6 @@ public class Rational extends Number implements Comparable<Rational> {
         }
     }
 
-    public Rational(float f) {
-        long bits = Float.floatToIntBits(f);
-
-        long sign = bits >>> 63;
-        long exponent = ((bits >>> 52) ^ (sign << 11)) - 1023;
-        long fraction = bits << 12; // bits are "reversed" but that's not a problem
-
-        long a = 1L;
-        long b = 1L;
-
-        for (int i = 63; i >= 12; i--) {
-            a = a * 2 + ((fraction >>> i) & 1);
-            b *= 2;
-        }
-
-        if (exponent > 0)
-            a *= 1L << exponent;
-        else
-            b *= 1L << -exponent;
-
-        if (sign == 1)
-            a *= -1;
-
-        num = (int) a;
-        den = (int) b;
-        new Rational(num, den);
-    }
-
     public Rational(String s) {
         int indexOfDivide = s.indexOf('/');
         if (indexOfDivide == -1) {
